428f0f908856c9b9a413cb9fd568da1de41369ce
apsgk9 <apsgk9@gmail.com>
2021-01-09 01:49:11 -0500

    Refactored and rearchitectured freelookzoom so that Value can be changed externally.

--- Assets/Scripts/Camera/CinemachineFreeLookZoom.cs
+++ Assets/Scripts/Camera/CinemachineFreeLookZoom.cs
@@ -1,125 +1,125 @@
 ﻿using System;
+using Cinemachine;
 using Cinemachine.Utility;
 using UnityEngine;
 
-namespace Cinemachine
+[RequireComponent(typeof(CinemachineFreeLook))]
+class CinemachineFreeLookZoom : MonoBehaviour
 {
-    [RequireComponent(typeof(CinemachineFreeLook))]
-    class CinemachineFreeLookZoom : MonoBehaviour
+    [Tooltip("The value of the input axis.  A value of 0 means no input.  You can drive "
+        + "this directly from a custom input system.")]
+    public float Value = 0f;
+    private CinemachineFreeLook freelook;
+    private CinemachineFreeLook.Orbit[] originalOrbits;
+    [Tooltip("The minimum scale for the orbits")]
+    [Range(0.01f, 1f)]
+    public float minScale = 0.5f;
+
+    [Tooltip("The maximum scale for the orbits")]
+    [Range(1F, 5f)]
+    public float maxScale = 1.5f;
+
+    public float CurrentScale = 1f;
+
+    [SaveDuringPlay]
+    [Tooltip("How fast should zoom should be")]
+    [Range(0.01f, 1f)]
+    public float Multiplier = 1f;
+
+
+
+    [SaveDuringPlay]
+    [Tooltip("The amount of time in seconds it takes to accelerate to a higher speed")]
+    public float accelTime;
+    [SaveDuringPlay]
+
+    [Tooltip("The amount of time in seconds it takes to decelerate to a lower speed")]
+    public float decelTime;
+
+
+    /// Internal state
+    private float mCurrentSpeed;
+    const float Epsilon = UnityVectorExtensions.Epsilon;
+    void OnValidate()
     {
-        private CinemachineFreeLook freelook;
-        private CinemachineFreeLook.Orbit[] originalOrbits;
-        [Tooltip("The minimum scale for the orbits")]
-        [Range(0.01f, 1f)]
-        public float minScale = 0.5f;
- 
-        [Tooltip("The maximum scale for the orbits")]
-        [Range(1F, 5f)]
-        public float maxScale = 1.5f;
-
-        [Range(0f, 1f)]
-        public float CurrentScale=1f;
-
-        [Tooltip("How fast should zoom should be")]
-        [Range(0.01f, 1f)]
-        public float Multiplier=1f;
-
-
-
-        [Tooltip("The amount of time in seconds it takes to accelerate to a higher speed")]
-        public float accelTime;
-
-        [Tooltip("The amount of time in seconds it takes to decelerate to a lower speed")]
-        public float decelTime;
-
-    
-        /// Internal state
-        private float mCurrentSpeed;
-        const float Epsilon =  UnityVectorExtensions.Epsilon;    
-        void OnValidate()
+        minScale = Mathf.Max(0.01f, minScale);
+        maxScale = Mathf.Max(minScale, maxScale);
+        accelTime = Mathf.Max(0, accelTime);
+        decelTime = Mathf.Max(0, decelTime);
+    }
+    void Awake()
+    {
+        freelook = GetComponentInChildren<CinemachineFreeLook>();
+        if (freelook != null)
         {
-            minScale = Mathf.Max(0.01f, minScale);
-            maxScale = Mathf.Max(minScale, maxScale);
-            accelTime = Mathf.Max(0, accelTime);
-            decelTime = Mathf.Max(0, decelTime);
-        } 
-        void Awake()
-        {
-            freelook = GetComponentInChildren<CinemachineFreeLook>();
-            if (freelook != null)
+            originalOrbits = new CinemachineFreeLook.Orbit[freelook.m_Orbits.Length];
+            for (int i = 0; i < originalOrbits.Length; i++)
             {
-                originalOrbits = new CinemachineFreeLook.Orbit[freelook.m_Orbits.Length];
-                for (int i = 0; i < originalOrbits.Length; i++)
-                {
-                    originalOrbits[i].m_Height = freelook.m_Orbits[i].m_Height;
-                    originalOrbits[i].m_Radius = freelook.m_Orbits[i].m_Radius;
-                }
-#if UNITY_EDITOR
-                SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreOriginalOrbits;
-                SaveDuringPlay.SaveDuringPlay.OnHotSave += RestoreOriginalOrbits;
-#endif
+                originalOrbits[i].m_Height = freelook.m_Orbits[i].m_Height;
+                originalOrbits[i].m_Radius = freelook.m_Orbits[i].m_Radius;
             }
-        }
- 
 #if UNITY_EDITOR
-        private void OnDestroy()
-        {
             SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreOriginalOrbits;
-        }
- 
-        private void RestoreOriginalOrbits()
-        {
-            if (originalOrbits != null)
-            {
-                for (int i = 0; i < originalOrbits.Length; i++)
-                {
-                    freelook.m_Orbits[i].m_Height = originalOrbits[i].m_Height;
-                    freelook.m_Orbits[i].m_Radius = originalOrbits[i].m_Radius;
-                }
-            }
-        }
+            SaveDuringPlay.SaveDuringPlay.OnHotSave += RestoreOriginalOrbits;
 #endif
- 
-        void Update()
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDestroy()
+    {
+        SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreOriginalOrbits;
+    }
+
+    private void RestoreOriginalOrbits()
+    {
+        if (originalOrbits != null)
         {
-            if (originalOrbits != null)
+            for (int i = 0; i < originalOrbits.Length; i++)
             {
-
-                float deltaScale = GetScaleChange(Time.deltaTime,ScrollValue());
-
-                CurrentScale+=deltaScale;
-                CurrentScale=Mathf.Clamp(CurrentScale,minScale,maxScale);
-
-                for (int i = 0; i < originalOrbits.Length; i++)
-                {
-                    freelook.m_Orbits[i].m_Height = originalOrbits[i].m_Height * CurrentScale;
-                    freelook.m_Orbits[i].m_Radius = originalOrbits[i].m_Radius * CurrentScale;
-                }
+                freelook.m_Orbits[i].m_Height = originalOrbits[i].m_Height;
+                freelook.m_Orbits[i].m_Radius = originalOrbits[i].m_Radius;
             }
         }
-
-        private float ScrollValue()
+    }
+#endif
+    
+    void Update()
+    {
+        if (originalOrbits != null)
         {
-            return UserInput.Instance.Scroll;
-        }
+            float deltaScale = DampenValue(Time.deltaTime, Value);
 
-        private float GetScaleChange(float deltaTime,float inputValue)
+            CurrentScale += deltaScale;
+            CurrentScale = Mathf.Clamp(CurrentScale, minScale, maxScale);
+
+            for (int i = 0; i < originalOrbits.Length; i++)
+            {
+                freelook.m_Orbits[i].m_Height = originalOrbits[i].m_Height * CurrentScale;
+                freelook.m_Orbits[i].m_Radius = originalOrbits[i].m_Radius * CurrentScale;
+            }
+        }
+    }
+    private void UpdateScrollValue(float input)
+    {
+        Value=input;
+    }    
+
+    private float DampenValue(float deltaTime, float inputValue)
+    {
+        float input = inputValue * Multiplier / 10f;
+        if (deltaTime < Epsilon)
         {
-            float input=inputValue* Multiplier/10f;
-            if (deltaTime < Epsilon)
-            {
-                mCurrentSpeed = 0;
-            }
-            else
-            {
-                float speed = input / deltaTime;
-                float dampTime = Mathf.Abs(speed) < Mathf.Abs(mCurrentSpeed) ? decelTime : accelTime;
-                speed = mCurrentSpeed + Damper.Damp(speed - mCurrentSpeed, dampTime, deltaTime);
-                mCurrentSpeed = speed;
-                input = speed * deltaTime;
-            }
-
-            return input;
+            mCurrentSpeed = 0;
         }
+        else
+        {
+            float speed = input / deltaTime;
+            float dampTime = Mathf.Abs(speed) < Mathf.Abs(mCurrentSpeed) ? decelTime : accelTime;
+            speed = mCurrentSpeed + Damper.Damp(speed - mCurrentSpeed, dampTime, deltaTime);
+            mCurrentSpeed = speed;
+            input = speed * deltaTime;
+        }
+        return input;
     }
 }
---
+++ Assets/Scripts/Camera/CinemachineFreeLookZoomController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineFreeLookZoom))]
+public class CinemachineFreeLookZoomController : MonoBehaviour
+{
+    private CinemachineFreeLookZoom freelookZoom;
+
+    private void Awake()
+    {
+        freelookZoom = GetComponentInChildren<CinemachineFreeLookZoom>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        freelookZoom.Value=0f;
+        UpdateDesktopValues();
+    }
+
+    private void UpdateDesktopValues()
+    {
+        freelookZoom.Value+=UserInput.Instance.Scroll;
+    }
+}
